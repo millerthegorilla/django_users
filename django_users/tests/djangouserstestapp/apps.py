dcca9fe64d2ac069b7a09e99678c6c624a1fbde4
james miller <jamesstewartmiller@gmail.com>
2022-08-31 17:35:12 +0100

    typo

--- django_users/tests/djangouserstestapp/apps.py
+++ django_users/tests/djangouserstestapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoUsersTestAppConfig(AppConfig):
     name = "djang_users.tests.testapp"
-    verbose_name = "DjangoUsersTestApp"
+    verbose_name = "DjangoUsersTestAppConfig"
