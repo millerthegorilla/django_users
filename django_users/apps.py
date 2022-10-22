00ddc455f41351d0f3b6a0daaa949162af8851a0
james miller <jamesstewartmiller@gmail.com>
2022-10-22 21:38:33 +0100

    experimenting with apps.ready

--- django_users/apps.py
+++ django_users/apps.py
@@ -10,7 +10,7 @@ from crispy_bootstrap5 import templates as bs5_templates
 from captcha import templates as captcha_templates
 
 
-APPS = [
+my_apps = [
     {"name": "crispy_forms", "templates": crispy_templates},
     {"name": "crispy_bootstrap5", "templates": bs5_templates},
     {"name": "captcha", "templates": captcha_templates},
@@ -23,12 +23,14 @@ class DjangoUsersConfig(AppConfig):
     name = "django_users"
 
     def ready(self) -> None:
-        for app in APPS:
+        global my_apps
+        for app in my_apps:
             if app["name"] not in settings.INSTALLED_APPS:
-                theapp = importlib.import_module(app["name"])
-                from theapp import apps as theapp_apps
-
-                theapp_apps.ready()
+                theapp = importlib.import_module(app["name"] + ".apps")
+                try:
+                    my_apps += theapp.my_apps
+                except AttributeError:
+                    pass
                 settings.INSTALLED_APPS += (app["name"],)
                 apps.app_configs = OrderedDict()
                 apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
