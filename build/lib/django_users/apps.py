e70f88239017c2f2cef887d7a3935d73e455b428
james miller <jamesstewartmiller@gmail.com>
2022-10-24 13:28:45 +0100

    end experimenting with apps.ready

--- build/lib/django_users/apps.py
+++ build/lib/django_users/apps.py
@@ -23,58 +23,59 @@ class DjangoUsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_users"
 
-def ready(self) -> None:
-        global my_apps
-        try:
-            if not settings.TOPLEVELCONFIG:
-                settings.TOPLEVELCONFIG = self.__class__
-                self.populate_my_apps()
-                self.install_apps()
-        except AttributeError:
-            settings.TOPLEVELCONFIG = self.__class__
-            self.populate_my_apps()
-            self.install_apps()
 
-    def populate_my_apps(self):
-        global my_apps
-        for app in my_apps:
-            try:
-                theapp = importlib.import_module(app["name"] + ".apps")
-                my_apps += [app for app in theapp.my_apps if app not in my_apps]
-                app["setup"] = theapp.setup_apps
-            except (ModuleNotFoundError, AttributeError):
-                pass
+# def ready(self) -> None:
+#         global my_apps
+#         try:
+#             if not settings.TOPLEVELCONFIG:
+#                 settings.TOPLEVELCONFIG = self.__class__
+#                 self.populate_my_apps()
+#                 self.install_apps()
+#         except AttributeError:
+#             settings.TOPLEVELCONFIG = self.__class__
+#             self.populate_my_apps()
+#             self.install_apps()
 
-    def install_apps(self):
-        for app in my_apps:
-            if app["name"] not in settings.INSTALLED_APPS:
-                settings.INSTALLED_APPS += (app["name"],)
-                apps.app_configs = OrderedDict()
-                apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
-                apps.clear_cache()
-                if app["templates"] != "":
-                    settings.TEMPLATES[0]["DIRS"].append(
-                        os.path.abspath(app["templates"].__path__._path[0])
-                    )
-                static = os.path.abspath(
-                    importlib.import_module(app["name"]).__path__[0] + "/static/"
-                )
-                if os.path.isdir(static):
-                    settings.STATICFILES_DIRS += [static]
-                apps.populate(settings.INSTALLED_APPS)
-                if "setup" in app:
-                    app["setup"]()
-        try:
-            self.setup_apps()
-        except AttributeError:
-            pass
+#     def populate_my_apps(self):
+#         global my_apps
+#         for app in my_apps:
+#             try:
+#                 theapp = importlib.import_module(app["name"] + ".apps")
+#                 my_apps += [app for app in theapp.my_apps if app not in my_apps]
+#                 app["setup"] = theapp.setup_apps
+#             except (ModuleNotFoundError, AttributeError):
+#                 pass
 
-    def setup_apps(self):
-        from django_email_verification import urls as email_urls  # include the urls
-        from django import urls
+#     def install_apps(self):
+#         for app in my_apps:
+#             if app["name"] not in settings.INSTALLED_APPS:
+#                 settings.INSTALLED_APPS += (app["name"],)
+#                 apps.app_configs = OrderedDict()
+#                 apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
+#                 apps.clear_cache()
+#                 if app["templates"] != "":
+#                     settings.TEMPLATES[0]["DIRS"].append(
+#                         os.path.abspath(app["templates"].__path__._path[0])
+#                     )
+#                 static = os.path.abspath(
+#                     importlib.import_module(app["name"]).__path__[0] + "/static/"
+#                 )
+#                 if os.path.isdir(static):
+#                     settings.STATICFILES_DIRS += [static]
+#                 apps.populate(settings.INSTALLED_APPS)
+#                 if "setup" in app:
+#                     app["setup"]()
+#         try:
+#             self.setup_apps()
+#         except AttributeError:
+#             pass
 
-        root_url = importlib.import_module(settings.ROOT_URLCONF)
-        if urls.path("email/", urls.include(email_urls)) not in root_url.urlpatterns:
-            root_url.urlpatterns.append(
-                urls.path("email/", urls.include(email_urls)),
-            )
+#     def setup_apps(self):
+#         from django_email_verification import urls as email_urls  # include the urls
+#         from django import urls
+
+#         root_url = importlib.import_module(settings.ROOT_URLCONF)
+#         if urls.path("email/", urls.include(email_urls)) not in root_url.urlpatterns:
+#             root_url.urlpatterns.append(
+#                 urls.path("email/", urls.include(email_urls)),
+#             )
